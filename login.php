7fc3015435d6b832cd6117a63a7d538e59fddd63
frnkln-olvrs <franklinoliveros2003@gmail.com>
2023-12-08 14:11:23 +0800

    login form

--- login.php
+++ login.php
@@ -5,21 +5,30 @@
 	include 'head.php';
 ?>
 <body>
-  <form>
-    <div class="mb-3">
-      <label for="exampleInputEmail1" class="form-label">Email address</label>
-      <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-      <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+  <main>
+    <div class="container-fluid vh-100 d-flex justify-content-center align-items-center">
+      <div class="login-page p4">
+        <p class="text-center">
+          <img src="./img/wmsu_logo.png" alt="wmsu-logo" class="img-fluid">
+        </p>
+        <h1 class="h2 my-3 mb-4 text-center brand-color">MyWMSU</h1>
+        <form action="" method="post">
+          <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Email address</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+          </div>
+
+          <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Password</label>
+            <input type="password" class="form-control" id="exampleInputPassword1">
+            <a href="#" id="forgot-pass" class="form-text d-flex justify-content-end">Forgot your password?</a>
+          </div>
+
+          <button type="submit" class="btn btn-primary">Submit</button>
+          <div id="emailHelp" class="form-text d-flex justify-content-center">Don't have an account?<a href="signup.php">Sign up</a></div>
+        </form>
+      </div>
     </div>
-    <div class="mb-3">
-      <label for="exampleInputPassword1" class="form-label">Password</label>
-      <input type="password" class="form-control" id="exampleInputPassword1">
-    </div>
-    <div class="mb-3 form-check">
-      <input type="checkbox" class="form-check-input" id="exampleCheck1">
-      <label class="form-check-label" for="exampleCheck1">Check me out</label>
-    </div>
-    <button type="submit" class="btn btn-primary">Submit</button>
-  </form>
+  </main>
 </body>
 </html>
