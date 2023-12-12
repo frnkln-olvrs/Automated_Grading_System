57092451a43556b59a8a13bc7f4dfff64035570e
frnkln-olvrs <franklinoliveros2003@gmail.com>
2023-12-12 11:20:45 +0800

    vendors import

--- head.php
+++ head.php
@@ -15,4 +15,5 @@
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
   <link rel="stylesheet" href="./vendor/bootstrap-5.0.2-dist/css/bootstrap.min.css">
   <script src="./vendor/bootstrap-5.0.2-dist/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="./vendor/bootstrap-5.0.2/css/bootstrap.min.css">
 </head>
