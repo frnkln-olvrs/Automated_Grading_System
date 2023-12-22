c48039a6ceb2ace3199bdbba31b75f6d97e25661
frnkln-olvrs <franklinoliveros2003@gmail.com>
2023-12-22 17:21:55 +0800

    minor add sub info table

--- js/subject_info-table.js
+++ js/subject_info-table.js
@@ -1,15 +1,24 @@
 $(document).ready(function(){
   dataTable = $("#subject-info").DataTable({
+    dom: 'Brtp',
+    scrollX: true,
+    pageLength: 10,
+    buttons: [
+      {
+        extend: 'excel',
+        split: [ 'pdf', 'csv'],
+      }
+    ],
     'columnDefs': [ {
-      'targets': [1,3,6,7], /* column index */
-      'orderable': false, /* true or false */
+        'targets': [1,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22], /* column index */
+        'orderable': false, /* true or false */
     }]
-  });
+});
 
   dataTable.buttons().container().appendTo($('#MyButtons'));
 
   var table = dataTable;
-  var filter = createFilter(table, [1,2,3]);
+  var filter = createFilter(table, [1,2,3,23]);
 
   function createFilter(table, columns) {
     var input = $('input#keyword').on("keyup", function() {
@@ -39,12 +48,7 @@ $(document).ready(function(){
 
   $('select#student-point_eqv').on('change', function(e){
     var status = $(this).val();
-    dataTable.columns([5]).search(status).draw();
-  });
-
-  $('select#student-remark').on('change', function(e){
-    var status = $(this).val();
-    dataTable.columns([6]).search(status).draw();
+    dataTable.columns([24]).search(status).draw();
   });
 })
 
--- subject-info.php
+++ subject-info.php
@@ -60,9 +60,27 @@
 
         <div class="content container-fluid mw-100 border rounded shadow p-3">
           
+          <div class="search-keyword col-12 flex-lg-grow-0 d-flex">
+            <div id="MyButtons" class="d-flex me-4 mb-md-2 mb-lg-0 col-12 col-md-auto"></div>
+              <div class="form-group me-2 col-12 col-sm-auto flex-sm-grow-1 flex-lg-grow-0 ms-lg-auto">
+                <select name="student-point_eqv" id="student-point_eqv" class="form-select me-md-2">
+                  <option value="">Point Eqv.</option>
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="5">5</option>
+                  
+                </select>
+              </div>
+            <div class="input-group">
+              <input type="text" name="keyword" id="keyword" placeholder="Search Product" class="form-control">
+              <button class="btn btn-outline-secondary brand-bg-color" type="button"><i class='bx bx-search' aria-hidden="true" ></i></button>
+            </div>
+            <a href="./add_student.php" class="btn btn-outline-secondary btn-add ms-3 brand-bg-color" type="button"><i class='bx bx-plus-circle'></i></a>
+          </div>
           
 
-          <table id="subject-info" class="table table-striped" style="width:300%">
+          <table id="subject-info" class="table table-striped" style="width:250%">
             <thead>
               <tr>
                 <th rowspan="5">#</th>
@@ -164,6 +182,39 @@
                 <td>22</td>
                 <td>15.33</td>
                 <td>4.6</td>
+  
+                <td>99</td>
+                <td>99.00</td>
+                <td>49.9</td>
+  
+                <td>80</td>
+                <td>2.50</td>
+              </tr>
+
+              <tr>
+                <td>2</td>
+                <td>2021-00150</td>
+                <td>Franklin Oliveros</td>
+                <td>zxc@email.com</td>
+                <td>1</td>
+  
+                <td>10</td>
+                <td>19</td>
+                <td>29</td>
+                <td>19.33</td>
+                <td>5.6</td>
+  
+                <td>9</td>
+                <td>15</td>
+                <td>22</td>
+                <td>15.33</td>
+                <td>4.6</td>
+  
+                <td>10</td>
+                <td>20</td>
+                <td>30</td>
+                <td>20</td>
+                <td>1</td>
   
                 <td>99</td>
                 <td>99.00</td>
