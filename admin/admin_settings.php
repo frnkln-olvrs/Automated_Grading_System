d89bec7074936ece2dc481320b013fb6eb9929a5
Frankii <franklinoliveros2003@gmail.com>
2024-03-22 18:43:44 +0800

    password dropdown

--- admin/admin_settings.php
+++ admin/admin_settings.php
@@ -38,7 +38,7 @@ if (!isset($_SESSION['user_role']) || (isset($_SESSION['user_role']) && $_SESSIO
       </div>
 
       <div class="m-4">
-        <form action="./curri_page.php">
+        <form action="">
           <div class="container-fluid d-flex justify-content-start">
             <span class="fs-2 fw-bold h1 m-0 brand-color mb-3">Change Password</span>
           </div>
@@ -53,17 +53,15 @@ if (!isset($_SESSION['user_role']) || (isset($_SESSION['user_role']) && $_SESSIO
                         <i class='bx bxs-camera-plus'></i>
                         <span>Change Image</span>
                       </label>
-                      <!-- <img src="./img/dataimages/<?= $_SESSION['user_profile'] ?>" id="output" /> -->
                       <img src="../img/profile-img/profile.png" id="output"/>
                       <input id="file" type="file" name="profile" accept="image/png, image/jpeg" onchange="validateFile(event)" />
                     </div>
-                    <div class="d-flex justify-content-center align-items-center">
-                      <button type="submit" name="saveimage" class="btn brand-bg-color">Save Image</button>
+                    <div class="name d-flex justify-content-center align-items-center">
+                      <p class="username fw-bold"> <?= ucwords($_SESSION['name']) ?> </p>
                     </div>
-                    <br>
-                    <div class="name" style="display: flex; justify-content: center; align-items: center;">
-                      <p class="username"> <?= ucwords($_SESSION['name']) ?> </p>
-                    </div>
+                    <!-- <div class="d-flex justify-content-center align-items-center">
+                      <button type="button" name="saveimage" class="btn brand-bg-color">Save Image</button>
+                    </div> -->
                   </form>
                 </div>
 
@@ -74,18 +72,29 @@ if (!isset($_SESSION['user_role']) || (isset($_SESSION['user_role']) && $_SESSIO
                 <label for="username" class="form-label">Username</label>
                 <input type="text" class="form-control" id="username" aria-describedby="username">
               </div>
-              <div class="mb-3">
-                <label for="current_pw" class="form-label">Current Password</label>
-                <input type="text" class="form-control" id="current_pw" aria-describedby="current_pw">
+              <div class="mb-2">
+                <button type="button" class="btn btn-toggle link-dark d-flex align-items-center nav-link p-0" data-bs-toggle="collapse" data-bs-target="#pw_input_toggle" aria-expanded="false">
+                  <i class='bx bxs-key me-2'></i>
+                  <span>Change password</span>
+                </button>
               </div>
-              <div class="mb-3">
-                <label for="new_pw" class="form-label">New Password</label>
-                <input type="text" class="form-control" id="new_pw" aria-describedby="new_pw">
+
+              <div class="collapse" id="pw_input_toggle">
+                <div class="mb-3">
+                  <label for="current_pw" class="form-label">Current Password</label>
+                  <input type="text" class="form-control" id="current_pw" aria-describedby="current_pw">
+                </div>
+                <div class="mb-3">
+                  <label for="new_pw" class="form-label">New Password</label>
+                  <input type="text" class="form-control" id="new_pw" aria-describedby="new_pw">
+                </div>
+                <div class="mb-3">
+                  <label for="confirm_pw" class="form-label">Confirm Password</label>
+                  <input type="text" class="form-control" id="confirm_pw" aria-describedby="confirm_pw">
+                </div>
               </div>
-              <div class="mb-3">
-                <label for="confirm_pw" class="form-label">Confirm Password</label>
-                <input type="text" class="form-control" id="confirm_pw" aria-describedby="confirm_pw">
-              </div>
+              
+              
             </div>
           </div>
           <div class="d-flex justify-content-end gap-2">
@@ -99,7 +108,6 @@ if (!isset($_SESSION['user_role']) || (isset($_SESSION['user_role']) && $_SESSIO
   </div>
 
   <script src="./js/main.js"></script>
-  <script src="./js/profiling_table.js"></script>
 
   <script>
     var validateFile = function(event) {
