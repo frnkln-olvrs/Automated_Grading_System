942c828829283bd11badb1f688f1c35d181eec7d
Frankii <franklinoliveros2003@gmail.com>
2024-08-07 09:52:20 +0800

    Update admin_sidepanel.php

--- includes/admin_sidepanel.php
+++ includes/admin_sidepanel.php
@@ -7,76 +7,78 @@
   <ul class="nav nav-pills flex-column mb-auto">
     <li class="nav-item">
       <a href="./index" class="nav-link link-dark d-flex align-items-center mb-2 <?= $curriculum_page ?>" aria-current="page">
-        <i class='bx bxs-graduation fs-4'></i>
-        <span class="fs-6 ms-2">School Year</span>
+        <i class='bx bxs-book-alt fs-4'></i>
+        <span class="fs-6 ms-2">Curriculum</span>
       </a>
     </li>
 
     <li class="nav-item">
-      <div class="btn-group d-flex flex-column">
+    <div class="btn-group d-flex flex-column">
         <div class="link-grp d-flex justify-content-between gap-1">
-          <a href="./profiling.php" class="nav-link link-dark d-flex align-items-center mb-2 w-100 <?= $profiling_page ?>" type="button">
-            <i class='bx bxs-dashboard fs-4'></i>
-            <span class="fs-6 ms-2">Profiling</span>
-          </a>
-          <button class="btn btn-toggle link-dark d-flex align-items-center mb-2 nav-link <?= $profiling_page ?> " data-bs-toggle="collapse" data-bs-target="#profiling_toggle" aria-expanded="false">
-            <i class='bx bx-chevron-down'></i>
-          </button>
+            <a href="./profiling.php" class="nav-link link-dark d-flex align-items-center mb-2 w-100 <?= $profiling_page ?>" type="button">
+                <i class='bx bxs-user-detail fs-4'></i>
+                <span class="fs-6 ms-2">Profiling</span>
+            </a>
+            <button class="btn btn-toggle link-dark d-flex align-items-center mb-2 nav-link <?= $profiling_page ?>" data-bs-toggle="collapse" data-bs-target="#profiling_toggle" aria-expanded="false">
+                <i class='bx bx-chevron-down'></i>
+            </button>
         </div>
 
-        
         <?php
-          require_once '../classes/department.class.php';
-          require_once '../tools/functions.php';
+        require_once '../classes/department.class.php';
+        require_once '../tools/functions.php';
 
-          $department = new Department();
+        $department = new Department();
+        $department_array = $department->show();
 
-          $department_array = $department->show();
-
-          $show_collapse = false; 
-          if (isset($_GET['department_id'])) {
-            $show_collapse = true; 
-          }
-        
-          require_once '../classes/department.class.php';
-          require_once '../tools/functions.php';
-        
-          $department = new Department();
-        
-          $department_array = $department->show();
+        $show_collapse = false;
+        if (isset($_GET['department_id'])) {
+            $show_collapse = true;
+        }
         ?>
 
         <div class="collapse<?= ($show_collapse ? ' show' : '') ?>" id="profiling_toggle" style="">
-          <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-            <?php
-              if ($department_array) {
-                foreach ($department_array as $item) {
-                  $active = false;
-                  
-                  if (isset($_GET['department_id']) && $_GET['department_id'] == $item['department_id']) {
-                    $active = true;
-                  }
-            ?>
-            <li>
-              <a href="./profiling.php?department_id=<?= $item['department_id'] ?>" class="link-dark nav-link<?= ($active ? ' active' : '') ?>">
-                <i class='bx bx-git-commit'></i>
-                <span class="fs-6 ms-2"><?= $item['department_name'] ?></span>
-              </a>
-            </li>
-            <?php
+            <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+                <?php
+                if ($department_array) {
+                    foreach ($department_array as $item) {
+                        $active = false;
+
+                        if (isset($_GET['department_id']) && $_GET['department_id'] == $item['department_id']) {
+                            $active = true;
+                        }
+
+                        // Assign the correct icon based on the department name
+                        $icon = '';
+                        if ($item['department_name'] == 'Department of Computer Science') {
+                            $icon = 'bx bx-code-alt fs-4';
+                        } elseif ($item['department_name'] == 'Department of Information Technology') {
+                            $icon = 'bx bxs-network-chart fs-4';
+                        } else {
+                            $icon = 'bx bx-git-commit fs-4';
+                        }
+                        ?>
+                        <li>
+                            <a href="./profiling.php?department_id=<?= $item['department_id'] ?>" class="link-dark nav-link d-flex align-items-center <?= ($active ? ' active' : '') ?>">
+                                <i class='<?= $icon ?>'></i>
+                                <span class="fs-6 ms-2"><?= $item['department_name'] ?></span>
+                            </a>
+                        </li>
+                        <?php
+                    }
                 }
-              }
-            ?>
-          </ul>
+                ?>
+            </ul>
         </div>
-      </div>
-    </li>
+    </div>
+</li>
+
 
     <li class="nav-item">
       <div class="btn-group d-flex flex-column">
         <div class="link-grp d-flex justify-content-between gap-1">
           <a href="./manage_acc.php" class="nav-link link-dark d-flex align-items-center mb-2 w-100 <?= $manage_acc ?>" type="button">
-            <i class='bx bx-user fs-4'></i>
+            <i class='bx bxs-cog fs-4'></i>
             <span class="fs-6 ms-2">Manage Account</span>
           </a>
           <button class="btn btn-toggle link-dark d-flex align-items-center mb-2 nav-link <?= $manage_acc ?> " data-bs-toggle="collapse" data-bs-target="#mnge_acc_toggle" aria-expanded="false">
@@ -94,7 +96,7 @@
             </li> -->
             <li>
               <a href="./user_acc.php" class="link-dark nav-link <?= $user_acc ?>">
-                <i class='bx bx-git-commit'></i>
+                <i class='bx bxs-user-circle fs-4'></i>
                 <span class="fs-6 ms-2">User Account</span>
               </a>
             </li>
@@ -107,7 +109,7 @@
       <div class="btn-group d-flex flex-column">
         <div class="link-grp d-flex justify-content-between gap-1">
           <a href="./faculty.php" class="nav-link link-dark d-flex align-items-center mb-2 w-100 <?= $faculty_page ?>">
-            <i class='bx bxs-user-circle fs-4' ></i>
+            <i class='bx bxs-group fs-4'></i>
             <span class="fs-6 ms-2">Faculty</span>          
           </a>
           <button class="btn btn-toggle link-dark d-flex align-items-center mb-2 nav-link <?= $faculty_page ?> " data-bs-toggle="collapse" data-bs-target="#faculty_toggle" aria-expanded="false">
@@ -119,13 +121,13 @@
           <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
             <li>
               <a href="#" class="link-dark nav-link <?= $comci_page ?>">
-                <i class='bx bx-git-commit'></i>
+                <i class='bx bx-code-alt fs-4' ></i>
                 <span class="fs-6 ms-2">Department of Computer Science</span>
               </a>
             </li>
             <li>
               <a href="#" class="link-dark nav-link <?= $it_page ?>">
-                <i class='bx bx-git-commit'></i>
+                <i class='bx bxs-network-chart fs-4'></i>
                 <span class="fs-6 ms-2">Department of Information Technology</span>
               </a>
             </li>
@@ -136,14 +138,14 @@
 
     <li class="nav-item">
       <a href="./grade" class="nav-link link-dark d-flex align-items-center mb-2  <?= $grade_page ?>">
-        <i class='bx bx-library fs-4'></i>
+        <i class='bx bxs-graduation fs-4'></i>
         <span class="fs-6 ms-2">Grade</span>          
       </a>
     </li>
     <li class="nav-item">
       <a href="./admin_settings" class="nav-link link-dark d-flex align-items-center mb-2  <?= $setting_page ?>">
         <i class='bx bx-cog fs-4'></i>
-        <span class="fs-6 ms-2">Setting</span>          
+        <span class="fs-6 ms-2">Settings</span>          
       </a>
     </li>
   </ul>
